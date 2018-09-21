b08cc18ec733a2ce905f635a59847f3394ffe30e
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-09-21 12:22:12 -0400

    items can be seen on each show page

--- Shopping-Lists/routes/users.js
+++ Shopping-Lists/routes/users.js
@@ -19,7 +19,6 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   User.findById(req.params.id)
   .then((user) => {
-    console.log(user.shoppingLists)
     res.render('users/show', {
       user
       
