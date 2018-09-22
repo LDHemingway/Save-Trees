c0584a4e2294b50453ab4829e58d3e14ff0fc123
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-09-22 12:43:54 -0400

    go back to lists link works

--- Shopping-Lists/routes/shopping-lists.js
+++ Shopping-Lists/routes/shopping-lists.js
@@ -1,18 +1,80 @@
 var express = require('express');
-var router = express.Router();
-const { User } = require('../db/schema')
-const { ShoppingList } = require('../db/schema')
+var router = express.Router({
+  mergeParams: true
+});
+const {
+  User,
+  ShoppingList,
+  Item
+} = require('../db/schema')
 
 
+
+// NEW LIST
+router.get('/new', (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      res.render('shoppingLists/new', {
+        user
+      })
+    })
+})
 
 // SHOW ONE
-router.get('/:id', function(req, res, next) {
-    ShoppingList.findById(req.params.id)
-    .then((shoppingLists) => {
-      res.render('shoppingLists/show.hbs', { shoppingLists });
+router.get('/:id', function (req, res, next) {
+  User.findById(req.params.userId)
+    .then((user) => {
+      const shoppingList = user.shoppingLists.id(req.params.id)
+      res.render('shoppingLists/show', { user, 
+        shoppingList
+      });
     })
-  });
+});
+
+//CREATE 
+router.post('/', (req, res) => {
+  console.log('OMG ITS HAPPENING', req.body)
+  const itemOne = new Item({
+    name: req.body.items[0],
+    quantity: req.body.quantity[0]
+  })
+  const itemTwo = new Item({
+    name: req.body.items[1],
+    quantity: req.body.quantity[1]
+  })
+  const itemThree = new Item({
+    name: req.body.items[2],
+    quantity: req.body.quantity[2]
+  })
+  const itemFour = new Item({
+    name: req.body.items[3],
+    quantity: req.body.quantity[3]
+  })
+  const listItems = [itemOne, itemTwo, itemThree, itemFour]
+  const newList = new ShoppingList({
+    name: req.body.name,
+    items: listItems
+  })
+  User.findById(req.params.userId)
+    .then((user) => {
+      console.log('FOUND USER', user, newList)
+      user.shoppingLists.push(newList)
+      return user.save()
+    })
+    .then((user) => {
+      res.redirect(`/users/${req.params.userId}`)
+    })
+})
+
+// DELETE LIST
+router.delete('/:id', (req, res) => {
+  userId = User.findById(req.params.userId)
+  ShoppingList.remove(req.params.id)
+  .then (() => {
+    console.log('deleted')
+    res.redirect('/users/userId')
+  })
+})
 
 
-
-  module.exports = router;
+module.exports = router;
